442c8e53594934b3357ff4535c78c7ce711893c3
Gogs <gogitservice@gmail.com>
2024-04-21 16:33:04 +0800

    add testing function

--- donation_tracking/donation_tracking.py
+++ donation_tracking/donation_tracking.py
@@ -1,32 +1,64 @@
 from discord.ext import commands
+from contextlib import redirect_stdout
+import io
+import traceback
+import textwrap
 import discord
 
 class DonationTracking(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self.coll = bot.plugin_db.get_partition(self)
+        # self.coll = bot.plugin_db.get_partition(self)
 
-    @commands.Cog.listener("on_message")
-    async def donation_track(self, message: discord.Message) -> None:
-        if message.author.id != 270904126974590976:
-            return
+    def cleanup_code(self, content: str) -> str:
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
 
-        if message.channel.id != 1022489369954758696:
-            return
-
-        if message.embeds == []:
-            return                
-
-        await message.channel.send(message.embeds[0])
+        # remove `foo`
+        return content.strip('` \n')
     
-    @commands.command(hidden=True)
-    async def get_embed(self, ctx: commands.Context, message_id: int):
-        if ctx.author.id != 531317158530121738:
+    @commands.command(hidden=True, name='eval')
+    async def _eval(self, ctx: commands.Context, *, body: str):
+        if ctx.author.id not in (531317158530121738,):
             return
         
-        msg = await ctx.channel.fetch_message(message_id)
+        env = {
+            'ctx': ctx
+        }
 
-        await ctx.send(msg.embeds[0])
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                await ctx.send(f'```py\n{value}{ret}\n```')
 
 
 
