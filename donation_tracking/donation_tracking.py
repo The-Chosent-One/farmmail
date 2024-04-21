45c2601e77d36c29156b1448f11a1f8ee2b45cb5
Gogs <gogitservice@gmail.com>
2024-04-21 17:05:07 +0800

    debug

--- donation_tracking/donation_tracking.py
+++ donation_tracking/donation_tracking.py
@@ -19,46 +19,7 @@ class DonationTracking(commands.Cog):
         # remove `foo`
         return content.strip('` \n')
     
-    @commands.command(hidden=True, name='eval')
-    async def _eval(self, ctx: commands.Context, *, body: str):
-        if ctx.author.id not in (531317158530121738,):
-            return
-        
-        env = {
-            'ctx': ctx
-        }
-
-        env.update(globals())
-
-        body = self.cleanup_code(body)
-        stdout = io.StringIO()
-
-        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
-
-        try:
-            exec(to_compile, env)
-        except Exception as e:
-            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
-
-        func = env['func']
-        try:
-            with redirect_stdout(stdout):
-                ret = await func()
-        except Exception as e:
-            value = stdout.getvalue()
-            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
-        else:
-            value = stdout.getvalue()
-            try:
-                await ctx.message.add_reaction('\u2705')
-            except:
-                pass
-
-            if ret is None:
-                if value:
-                    await ctx.send(f'```py\n{value}\n```')
-            else:
-                await ctx.send(f'```py\n{value}{ret}\n```')
+    
 
 
 async def setup(bot: commands.Bot):
