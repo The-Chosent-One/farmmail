caa83b38941b417251b8293a03cfe38c03bf6c24
The-Chosent-One <57739806+The-Chosent-One@users.noreply.github.com>
2024-04-23 16:12:50 +0800

    Testing cases.py

--- donation_tracking/donation_tracking.py
+++ donation_tracking/donation_tracking.py
@@ -10,55 +10,26 @@ class DonationTracking(commands.Cog):
         self.bot = bot
         self.coll = bot.plugin_db.get_partition(self)
 
-    def cleanup_code(self, content: str) -> str:
-        """Automatically removes code blocks from the code."""
-        # remove ```py\n```
-        if content.startswith('```') and content.endswith('```'):
-            return '\n'.join(content.split('\n')[1:-1])
-
-        # remove `foo`
-        return content.strip('` \n')
-    
-    @commands.command(hidden=True, name='eval')
-    async def _eval(self, ctx: commands.Context, *, body: str):
-        if ctx.author.id not in (531317158530121738,):
+    @commands.Cog.listener("on_message")
+    async def donation_track(self, message: discord.Message) -> None:
+        if message.author.id != 270904126974590976:
             return
-        
-        env = {
-            'ctx': ctx
-        }
 
-        env.update(globals())
+        if message.embeds == []:
+            return
 
-        body = self.cleanup_code(body)
-        stdout = io.StringIO()
+        if message.embeds[0].description != "Successfully donated!":
+            return
 
-        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+        if message.channel.id != 1022489369954758696:
+            return
 
-        try:
-            exec(to_compile, env)
-        except Exception as e:
-            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+        original = message.reference.resolved
 
-        func = env['func']
-        try:
-            with redirect_stdout(stdout):
-                ret = await func()
-        except Exception as e:
-            value = stdout.getvalue()
-            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
-        else:
-            value = stdout.getvalue()
-            try:
-                await ctx.message.add_reaction('\u2705')
-            except:
-                pass
+        if original is None or isinstance(original, discord.DeletedReferencedMessage):
+            return
 
-            if ret is None:
-                if value:
-                    await ctx.send(f'```py\n{value}\n```')
-            else:
-                await ctx.send(f'```py\n{value}{ret}\n```')
+        await message.channel.send(original.embeds[0].description)
 
 
 async def setup(bot: commands.Bot):
