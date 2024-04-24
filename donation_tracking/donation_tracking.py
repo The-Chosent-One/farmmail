91bab3445060e6b5f4981072f2d338bf9166a6ba
The-Chosent-One <57739806+The-Chosent-One@users.noreply.github.com>
2024-04-24 11:03:20 +0800

    Draft 1.04

--- donation_tracking/donation_tracking.py
+++ donation_tracking/donation_tracking.py
@@ -23,30 +23,34 @@ class DonationTracking(commands.Cog):
 
         if message.channel.id != 1022489369954758696:
             return
+
+        if "Are you sure you want to donate your" not in message.embeds[0].description:
+            return
             
-        if message.embeds[0].description != "Successfully donated!":
-            return
+        # if message.embeds[0].description != "Successfully donated!":
+        #     return
         
-        original = message.reference.cached_message or message.reference.resolved
+        # original = message.reference.cached_message or message.reference.resolved
 
-        if isinstance(original, discord.DeletedReferencedMessage):
-            return
+        # if isinstance(original, discord.DeletedReferencedMessage):
+        #     return
 
-        if original is None:
-            original = await message.channel.fetch_message(message.reference.message_id)
+        # if original is None:
+        #     original = await message.channel.fetch_message(message.reference.message_id)
 
+        original = message
         donator_id = original.interaction.user.id
         donation_msg = original.embeds[0].description
         
         if "⏣" in donation_msg:
             coins_donated = int(self.coins_re.findall(donation_msg)[0].replace(",", "_"))
-            self.coll.update_one({"user_id": donator_id}, {"$inc", {"dank_coins": coins_donated}}, upsert=True)
+            await self.coll.update_one({"user_id": donator_id}, {"$inc", {"dank_coins": coins_donated}}, upsert=True)
             await message.channel.send(f"You have donated ⏣ {coins_donated}... I think")
 
         else:
             number_of_items, item = self.items_re.findall(donation_msg)
             number_of_items = int(number_of_items.replace(",", "_"))
-            self.coll.update_one({"user_id": donator_id}, {"$inc", {f"items.{item}": number_of_items}}, upsert=True)
+            await self.coll.update_one({"user_id": donator_id}, {"$inc", {f"items.{item}": number_of_items}}, upsert=True)
             await message.channel.send(f"You have donated {number_of_items} {item}... hopefully")
 
 async def setup(bot: commands.Bot):
