968b3fc32d6810057bbace76fc0e814f0a4e0ba8
The-Chosent-One <57739806+The-Chosent-One@users.noreply.github.com>
2024-04-24 10:56:53 +0800

    Draft 1.03

--- donation_tracking/donation_tracking.py
+++ donation_tracking/donation_tracking.py
@@ -23,22 +23,18 @@ class DonationTracking(commands.Cog):
 
         if message.channel.id != 1022489369954758696:
             return
-
-        if "Are you sure you want to donate your" not in message.embeds[0].description:
-            return
             
-        # if message.embeds[0].description != "Successfully donated!":
-        #     return
+        if message.embeds[0].description != "Successfully donated!":
+            return
         
-        # original = message.reference.cached_message or message.reference.resolved
+        original = message.reference.cached_message or message.reference.resolved
 
-        # if isinstance(original, discord.DeletedReferencedMessage):
-        #     return
+        if isinstance(original, discord.DeletedReferencedMessage):
+            return
 
-        # if original is None:
-        #     original = await message.channel.fetch_message(message.reference.message_id)
+        if original is None:
+            original = await message.channel.fetch_message(message.reference.message_id)
 
-        original = message
         donator_id = original.interaction.user.id
         donation_msg = original.embeds[0].description
         
